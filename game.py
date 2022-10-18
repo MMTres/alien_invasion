c8a86b6de00e68487dd8cbe58212fb7008a28689
MMTres <116024541+MMTres@users.noreply.github.com>
2022-10-18 08:48:34 -0400

    created settings class

--- game.py
+++ game.py
@@ -1,16 +1,16 @@
 import pygame
 import sys
+from settings import Settings
 
 class AlienInvasion:
     """Overall class to manage game assets and behavior"""
     def __init__(self):
         """Initialize the game, and create game resources"""
         pygame.init()
-        self.screen = pygame.display.set_mode((1200,800))
+        self.settings = Settings()
+        self.screen = pygame.display.set_mode((self.settings.screen_width, self.settings.screen_height))
         pygame.display.set_caption("Alien Invasion")
 
-        #set the background color
-        self.bg_color = (230,230,230)
     def run_game(self):
         """Start the main loop for the game"""
         while True:
@@ -19,7 +19,7 @@ class AlienInvasion:
                 if event.type == pygame.QUIT:
                     sys.exit()
             #redraw the screen with each pass through the loop
-            self.screen.fill(self.bg_color)
+            self.screen.fill(self.settings.bg_color)
             #make the most recently drawn screen visible
             pygame.display.flip()
 if __name__ == '__main__':
