87b1b7e5668c89156b8b3e4cdd8942fb7f345c3d
MMTres <116024541+MMTres@users.noreply.github.com>
2022-10-18 09:21:43 -0400

    added the ship to the screen

--- game.py
+++ game.py
@@ -1,6 +1,7 @@
 import pygame
 import sys
 from settings import Settings
+from ship import Ship
 
 
 class AlienInvasion:
@@ -10,8 +11,10 @@ class AlienInvasion:
         """Initialize the game, and create game resources"""
         pygame.init()
         self.settings = Settings()
+
         self.screen = pygame.display.set_mode((self.settings.screen_width, self.settings.screen_height))
         pygame.display.set_caption("Alien Invasion")
+        self.ship = Ship(self)
 
     def run_game(self):
         """Start the main loop for the game"""
@@ -22,6 +25,7 @@ class AlienInvasion:
                     sys.exit()
             # redraw the screen with each pass through the loop
             self.screen.fill(self.settings.bg_color)
+            self.ship.blitme()
             # make the most recently drawn screen visible
             pygame.display.flip()
 
